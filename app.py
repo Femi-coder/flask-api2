26697718a8fcd9cc99d110b17fd7036d168bede8
Femi-coder <147275459+Femi-coder@users.noreply.github.com>
2025-04-05 17:54:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def add_review():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-#  Run the Flask server
+# Run the Flask server
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
